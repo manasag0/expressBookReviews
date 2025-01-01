55303d2d3b40477b9c8b29aa28366c7364ca1c16
Manasa G <manasa.g7890@hotmail.com>
2025-01-01 16:53:56 +0530

    updated

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,44 +5,70 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  try{
+public_users.get('/', function (req, res) {
+  try {
     // get all books
     res.status(200).json(books);
-  }catch(err){
-    res.status(500).json({message: "Internal Server Error"});
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
   }
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+  try {
+    let isbn = req.params.isbn;
+    // Find the book by ISBN
+    let book = Object.values(books).find(book => book.isbn === isbn);
+    
+    if (book) {
+      res.status(200).json(book);
+      res.write(book)
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+  return res.status(300).json({ message: "Yet to be implemented" });
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    let authorName = req.params.author;
+    // Find books by author
+    let booksByAuthor = Object.values(books).filter(book => book.author === authorName);
+    if (booksByAuthor.length > 0) {
+      res.status(200).json(booksByAuthor);
+    } else {
+      res.status(404).json({ message: "Books by this author not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.general = public_users;
